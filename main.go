8478f905621ac2b48650ec1dbf1d4dcf82da0aec
Nikhil Ghosh <nikhil.ghosh@tala.co>
2018-07-31 16:25:57 -0700

    Add retry behavior specs

--- main.go
+++ main.go
@@ -32,3 +32,13 @@ func main() {
 	fmt.Println("starting (i.e., compilation successful)")
 	lambda.Start(kinesisHandler)
 }
+
+
+/* Retry behavior: 
+ * When a Lambda function invocation fails, AWS Lambda attempts to process the erring batch 
+ * of records until the time the data expires, which can be up to seven days.
+ * The exception is treated as blocking, and AWS Lambda will not read any new records from the shard 
+ * until the failed batch of records either expires or is processed successfully. This ensures that 
+ * AWS Lambda processes the stream events in order.
+ *
+ */
