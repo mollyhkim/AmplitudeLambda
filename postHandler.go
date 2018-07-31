71dda4fd8da6db88defd8660dd22b4bf1c23c568
Nikhil Ghosh <nikhil.ghosh@tala.co>
2018-07-31 15:29:05 -0700

    Add error interface and info retrieval functions

--- postHandler.go
+++ postHandler.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 	"bytes"
 	"io/ioutil"
+	"log"
+	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func postUserProperties(jsonStr string) {
@@ -17,6 +19,27 @@ func postEvent(jsonStr string) {
 
 /* TODO: Create a custom Status error type and integrate with Lambda
  */
+// Error has methods for HTTP status codes, embeds built-in error interface
+type Error interface {
+	error
+	Status() int
+}
+
+// Error with an HTTP status code
+type StatusError struct {
+	Code int
+	Err error
+}
+
+// Retrieve the http error from a StatusError
+func (s StatusError) Error() string {
+	return s.Err.error
+}
+
+// Return http code from a StatusError
+func (s StatusError) Status() int{
+	return s.code
+}
 
 // make HTTP post -- find API key in Amplitude project settings
 func sendPost(postType string, urlPath string, jsonStr string) {
