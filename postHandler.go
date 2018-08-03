db18ff7760f739b99a38556b3736ffe7700cb5e1
Nikhil Ghosh <nikhil.ghosh@tala.co>
2018-08-02 18:02:04 -0700

    Work in progress-- handling errors with HTTP

--- postHandler.go
+++ postHandler.go
@@ -6,6 +6,7 @@ import (
 	"bytes"
 	"io/ioutil"
 	"log"
+	"errors"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
@@ -32,25 +33,33 @@ func sendPost(postType string, urlPath string, jsonStr string) {
 	q.Add("api_key", "70f8f71d95f5a2910ee56e56f9d24756")
 	req.URL.RawQuery = q.Encode()
 
-/* TODO: Add functionality to differentiate between different HTTP errors (between Lambda<>Amplitude), example:
- * 400 - malformed json (bad request). Log info then return success/null/cary-on
- * 5xx - network error? Means amp is down...
- */
 	client := &http.Client{}
 	resp, err := client.Do(req)
+	// Non-2XX response does not cause an error. Error returned if too many redirects or HTTP protocol error.
+	// (see article)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Handle individual categories of responses
+	// TODO: Handle other 4XX (non-400) codes?
 	if resp.StatusCode != 200 {
 		switch e := resp.StatusCode {
 		case 400:
 			log.Print("Malformed JSON error")
+			// move along
 			return nil
+		case 500 <= e && e <= 511:
+			// e.g. Amp down (network error)
+			return errors.New("Server-side error")
 		default:
-			return err
+			return errors.New("HTTP Error %d. %s", e, resp)
 		}
 	}
 	defer resp.Body.Close()
 
 	fmt.Println("response Status:", resp.Status)
 	fmt.Println("response Headers:", resp.Header)
+	// we need to be careful about how we use ioutil.ReadAll, because of memory constraints
 	body, _ := ioutil.ReadAll(resp.Body)
 	fmt.Println("response Body:", string(body))
 }
